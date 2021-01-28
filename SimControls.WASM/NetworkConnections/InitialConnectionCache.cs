6b1ae444f70ede67bd6303cadaad85b04c51b9f4
John Melville <JohnMelville@gmail.com>
2021-01-28 05:30:41 -0500

    Does not work because WASM does not support UDP.

---
+++ SimControls.WASM/NetworkConnections/InitialConnectionCache.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Melville.P2P.Raw.BinaryObjectPipes;
+using Melville.P2P.Raw.Matchmaker;
+using SimControls.Model;
+
+namespace SimControls.WASM.NetworkConnections
+{
+    public interface IInitialConnectionCache
+    {
+        Task WaitForConnection();
+    }
+
+    public class InitialConnectionCache: ISimVariableBinder, IInitialConnectionCache
+    {
+        private ISimVariableBinder? innerBinder;
+        private readonly MatchMakerClient client;
+
+        private readonly Func<BinaryObjectPipeReader, BinaryObjectPipeWriter, ISimVariableBinder>
+            binderFactory;
+
+        public InitialConnectionCache(MatchMakerClient client)
+        {
+            this.client = client;
+        }
+
+        public void BindVariableToSimulator<T>(
+            string name, string unit, string simType, ReadOnlyDataItem<T> variable) => 
+            innerBinder?.BindVariableToSimulator(name, unit, simType, variable);
+
+        public void BindEventToSimulator(SimEventTrigger simEvent) =>
+            innerBinder?.BindEventToSimulator(simEvent);
+
+        public async Task WaitForConnection()
+        {
+            var (reader, writer) = await client.Connect();
+            innerBinder = binderFactory(reader, writer);
+        }
+    }
+}
--- SimControls.WASM/Program.cs
+++ SimControls.WASM/Program.cs
@@ -3,10 +3,17 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using Melville.P2P.Raw.BinaryObjectPipes;
+using Melville.P2P.Raw.Matchmaker;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SimControls.Model;
+using SimControls.NetworkCommon.DataClasses;
+using SimControls.NetworkCommon.NetworkVariableBinders;
+using SimControls.WASM.NetworkConnections;
 
 namespace SimControls.WASM
 {
@@ -17,9 +24,24 @@ namespace SimControls.WASM
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            RegisterVariableCache(builder.Services);
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
+        }
+
+        private static void RegisterVariableCache(IServiceCollection svc)
+        {
+            svc.AddSingleton<BinaryObjectDictionary, SimObjectDictionary>();
+            svc.AddSingleton<ISimVariableBinder, InitialConnectionCache>();
+            svc.AddSingleton<IInitialConnectionCache>(i =>
+                (IInitialConnectionCache) i.GetRequiredService<ISimVariableBinder>());
+            svc.AddSingleton<IVariableCache, VariableCache>();
+            svc.AddSingleton<Func<BinaryObjectPipeReader, BinaryObjectPipeWriter, ISimVariableBinder>>(
+                (reader,writer) => new NetworkVariableBinder(reader, writer));
+            svc.AddTransient<MatchMakerClient>(i =>
+                new MatchMakerClient(70, i.GetRequiredService<BinaryObjectDictionary>()));
         }
     }
 }
