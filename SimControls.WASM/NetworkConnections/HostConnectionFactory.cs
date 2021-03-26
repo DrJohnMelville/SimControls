fcede693448c9f76f6fb842ccae35d584a36f418
John Melville <JohnMelville@gmail.com>
2021-03-26 05:09:31 -0400

    Removed hard coded host Uri

--- SimControls.WASM/NetworkConnections/HostConnectionFactory.cs
+++ SimControls.WASM/NetworkConnections/HostConnectionFactory.cs
@@ -2,6 +2,7 @@
 using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using SimControls.Model.VariableBinders;
 
 namespace SimControls.WASM.NetworkConnections
@@ -11,18 +12,31 @@ namespace SimControls.WASM.NetworkConnections
         private readonly ICompositeVariableBinder rootBinder;
         private readonly Func<WebSocket, ISimVariableBinder>
             binderFactory;
+        private readonly NavigationManager navMgr;
 
-        public HostConnectionFactory(ICompositeVariableBinder rootBinder, Func<WebSocket, ISimVariableBinder> binderFactory)
+        public HostConnectionFactory(
+            ICompositeVariableBinder rootBinder, 
+            Func<WebSocket, ISimVariableBinder> binderFactory, 
+            NavigationManager navMgr)
         {
             this.rootBinder = rootBinder;
             this.binderFactory = binderFactory;
+            this.navMgr = navMgr;
         }
 
         public async Task ConnectToSeverAsync()
         {
             var socket = new ClientWebSocket();
-            await socket.ConnectAsync(new Uri("ws://192.168.0.17:5432"), CancellationToken.None);
+            await socket.ConnectAsync(HostWebSocketUri(), CancellationToken.None);
             rootBinder.AddBinder(binderFactory(socket));
         }
+
+        private Uri HostWebSocketUri() => ChangeSchemeToWebSocket(navMgr.ToAbsoluteUri("/"));
+
+        private static Uri ChangeSchemeToWebSocket(Uri host) =>
+            new UriBuilder(host)
+            {
+                Scheme = "ws"
+            }.Uri;
     }
    }
